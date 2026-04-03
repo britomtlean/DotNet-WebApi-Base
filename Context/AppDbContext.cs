e0a8b13ff629734f107185126845081af916d406
britomtlean <leandromatosqn@gmail.com>
2026-04-03 14:26:53 -0300

    first commit

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi2026.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
